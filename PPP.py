edce1b10ef30d33a33e9d0055d45bae6d095ac99
Cronjaeg <kitkat2.0@gmail.com>
2014-04-24 12:21:58 +0200

    Decided to take funtionality from PPP.py, related to the intensity-measure, and put it in a seperate class thai I have chosen to call intensityMeasure.py

--- PPP.py
+++ PPP.py
@@ -2,16 +2,17 @@ class PPP(object):
 	'A Possion Point Process, for driving Xi-coalescents'
 	
 	def __init__(self,intensityMeasure,n,T):
-		#self.t = 0.
+		self.measure = intensityMeasure
 		self.events = {}
-		#self.hasCoalesced = FALSE
-		#self.lineages = n
 		self.resample()
-
-	def resample(self):
-		while t <= T:
+		
+	#def resample(self):
+		#while t <= T:
 			#do stuff
 	
+	def __getitem__(self):
+		#do stuff
+
 	#def TMRCA(self):
 		#if self.hasCoalesced:
 			#return max(self.events.keys())
---
+++ intensityMeasure.py
@@ -0,0 +1,4 @@
+class intensityMeasure(object):
+    'An intensity measure for PossionProcesses supported measure types are Kingman-; and certain classes of Lambda-, and Xi-coalescents'
+    def __init__(self,measureType,*parameters):
+        pass

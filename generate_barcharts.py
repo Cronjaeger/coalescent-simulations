84033cd87f7947fc1caa8df3c3b623a91b5e2879
cronjaeger <kitkat2.0@gmail.com>
2015-06-11 18:02:15 +0100

    added code for generating barcharts in a separate file (actually add the file)

---
+++ generate_barcharts.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 17:31:49 2015
+
+@author: mathias
+"""
+
+import finiteSitesModell_investigations
+
+if __name__ == "__main__":
+    arglist = []
+    for L in [2, 8, 32, 128, 512, 2048, 8192]:
+#    for L in [2,8,32]:
+        for n in (2,8,32,128):
+            arglist.append([11,L,n])
+
+#    print arglist
+
+    finiteSitesModell_investigations.run_generatePlot_of_mutationTypes(arglist)

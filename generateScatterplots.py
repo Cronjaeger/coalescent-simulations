6f24d4990f1004cc358aa3bfd6732fd2bb81b96d
cronjaeger <kitkat2.0@gmail.com>
2015-06-12 09:16:09 +0100

    use Agg-backend for generating plots (does not require an X-server to be running)

--- generateScatterplots.py
+++ generateScatterplots.py
@@ -5,12 +5,14 @@ Created on Fri Jun 12 08:58:43 2015
 @author: mathias
 """
 
+import matplotlib
+matplotlib.use('Agg') #Use a backend that does not require an X-server to be running
 import finiteSitesModell_investigations
 
 if __name__ == "__main__":
     arglist = []
     for L in [2, 8, 32, 128, 512, 2048, 8192]:
         for n in (2,8,32,128):
-            arglist.append([1000,L,n])
+            arglist.append([10,L,n])
 
     finiteSitesModell_investigations.run_generateScatterplots(arglist)

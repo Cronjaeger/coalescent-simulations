4d8e77032b4bcd57af5fa4140805a73d72ad822b
cronjaeger <kitkat2.0@gmail.com>
2016-06-06 15:47:00 +0100

    generated new barcharts

--- generate_barcharts.py
+++ generate_barcharts.py
@@ -12,9 +12,9 @@ N_events_to_Stop_after = 1
 if __name__ == "__main__":
     arglist = []
     #for L in [2, 8, 32, 128, 512, 2048, 8192]:
-    # for L in [8,2048]:
-    #     for n in [8,128]:
-    #         arglist.append([1000,L,n])
+    for L in [8,64,128,512,2048]:
+        for n in [8,32,128]:
+            arglist.append([1000,L,n])
 
     # Add some BIG parameters (in case the computer terminated too early)
     arglist.append([1000,2048,8])

3af205309ff98dcc8cfd26199c7d2ea41d203f76
Mathias C. Cronjager <kitkat2.0@gmail.com>
2017-03-07 16:25:40 +0000

    Embellished summary and expanded options for plotting the results of recombination-graphs.

--- incompatability_graph_experiments.py
+++ incompatability_graph_experiments.py
@@ -145,15 +145,19 @@ def generate_seed():
     '''
     return int(np.random.sample(1)*(2**31))
 
-def ms_sim_theta(n, theta, N = 1, rho = 0.0 , nsites = 1000000, seed = generate_seed(), extra_args = []):
+def ms_sim_theta(n, theta, N = 1, rho = 0.0 , nsites = 1000000, seed = generate_seed(), with_trees = True, extra_args = []):
     if n<=1 or theta <0 or N<1: raise(ValueError('invalid input to ms_sim_theta: n=%i, theta=%f, N=%i.'%(n,theta,N)))
+    if with_trees:
+        extra_args.append('-T')
     args = ['ms',str(n),str(N),'-seeds',str(seed),'-t',str(theta),'-rho', str(rho), str(nsites)]+extra_args
     output_raw = run_sim(args)
     output_parsed = parse_ms_output(output_raw)
     return output_parsed
 
-def ms_sim_sites(n, s, N = 1, rho = 0.0, nsites = 1000000, seed = generate_seed(), extra_args = []):
+def ms_sim_sites(n, s, N = 1, rho = 0.0, nsites = 1000000, seed = generate_seed(), with_trees = True, extra_args = []):
     if n<=1 or s <1 or N<1: raise(ValueError('invalid input to ms_sim_sites: n=%i, s=%i, N=%i.'%(n,s,N)))
+    if with_trees:
+        extra_args.append('-T')
     args = ['ms',str(n),str(N),'-seeds',str(seed),'-s',str(s),'-rho', str(rho), str(nsites)] + extra_args
     output_raw = run_sim(args)
     output_parsed = parse_ms_output(output_raw)
@@ -164,6 +168,9 @@ def parse_ms_output(raw_input):
     raw = str(raw_input)
     lines = str.split(raw,'\n')
     chunks =  str.split(raw,r'//')
+    # print 'parsing raw input:'
+    # print raw
+    # print '='*80
     '''
     for refenrence, this is what a chunk looks like:
         ['',
@@ -178,9 +185,10 @@ def parse_ms_output(raw_input):
          '']
     '''
     S_list = []
+    trees_list = []
     positions_list = []
 
-    for chunk in chunks[1:]: #the first entry lists input,seed and trees (if applicable)
+    for i,chunk in enumerate(chunks[1:]): #the first entry lists input,seed and trees (if applicable)
 
         # add an extra empty line to the last line (this way all chunks terminate in two empty lines)
         if chunk == chunks[-1]:
@@ -191,8 +199,22 @@ def parse_ms_output(raw_input):
 
         #the 0th line is empty
 
-        #the 1st line contains the number of segregating sites
-        segsites_str = chunk_lines[1]
+        #the next lines represent trees (if they are simulated)
+        if chunk_lines[1][0] == 's':
+            n_trees = 0
+            trees = []
+            #print 'NO trees. chunk_lines[1]=%s'%chunk_lines[1]
+        else:
+            n_trees = 1
+            trees = [parse_tree_from_ms(chunk_lines[n_trees])]
+            while chunk_lines[n_trees+1][0] != 's':
+                n_trees += 1
+                trees.append(parse_tree_from_ms(chunk_lines[n_trees]))
+
+        trees_list.append(trees)
+
+        #the following line contains the number of segregating sites
+        segsites_str = chunk_lines[1+n_trees]
         n_segsites = parse_segsites(segsites_str)
 
         # when there are 0 segregating sites, there is nothing more to parse.
@@ -202,10 +224,10 @@ def parse_ms_output(raw_input):
 
         #when there are segregating sites, there is also output to parse
         else:
-            positions_str = chunk_lines[2]
+            positions_str = chunk_lines[2+n_trees]
             positions = parse_positions(positions_str)
 
-            rows_str = chunk_lines[3:-2] # these are the lines which correspond to output
+            rows_str = chunk_lines[(3+n_trees):-2] # these are the lines which correspond to output
             rows_arr = map(parse_row,rows_str) #each row is converted to a 1d array of integers
             S = np.r_[rows_arr]
 
@@ -217,14 +239,35 @@ def parse_ms_output(raw_input):
             'metadata':parse_metadata(chunks[0]),
             'S_list':S_list,
             'positions':positions_list,
+            'trees_list':trees_list,
             'n_segsites':n_segsites,
-            'experiments':zip(S_list,positions_list)}
+            'experiments':zip(S_list,positions_list,trees_list)}
 
 def parse_segsites(input_str):
+    # print 'parsing segsites from string: %s'%input_str
     return int(str.split(input_str)[1])
 
 def parse_positions(input_str):
+    # print 'parsing positions from string: %s'%input_str
     return map(float,str.split(input_str)[1:])
+
+def parse_tree_from_ms(input_str):
+    '''
+    takes a tree-string like '[10](1:0.201,(2:0.078,3:0.078):0.123);', and
+    outputs
+    '''
+    # print 'parsing tree from string: %s'%input_str
+    if input_str[0]=='[':
+        closing_bracket_position = input_str.index(']')
+        sites = int(input_str[1:closing_bracket_position])
+        newick_str = input_str[closing_bracket_position+1:]
+    elif input_str[0]=='(':
+        newick_str = input_str
+        sites = 'all'
+    else:
+        raise TypeError("cannot parse a tree from input string '%s'"%input_str)
+
+    return {'sites':sites,'str':newick_str}
 
 def parse_row(row_str):
     return np.array([int(i) for i in row_str], dtype = int)
@@ -333,6 +376,9 @@ def summary(S):
     stariness = compute_staryness(G)
     ranked_handshake_dist = ranked_handshake_distribution(G)
 
+    incompatible_groups = reduce(lambda x,y: x+y, [list(s) for s in nx.connected_components(G) if len(s) > 1], [])
+
+    S_only_incomplatible = S[:,incompatible_groups]
 
     summary_statistics = \
     {
@@ -344,7 +390,8 @@ def summary(S):
     'ranked_handshake_dist':ranked_handshake_dist,
     'chars as partitions on taxa':partitions,
     'state distribution':state_distribution,
-    'n informative chars':n_informative_columns
+    'n informative chars':n_informative_columns,
+    'S with only problematic characters':S_only_incomplatible
     }
 
 
@@ -453,7 +500,7 @@ def draw_FSM_graph(G, alpha = .2, base_node_size = 10, color_dict = None):
     pos = node_positions(G)
     nx.draw(G, nodelist = nodelist, width = 0.4, pos = pos, node_color = node_color, alpha = alpha, node_size = sizes, linewidths = 0.0)
 
-def draw_IS_w_recomb_graph(G, alpha = .2, base_node_size = 10):
+def draw_IS_w_recomb_graph(G, alpha = .2, base_node_size = 10, labels = {},font_size = 12):
 
     sizeDict = size_proportional_to_degree(G, base = base_node_size)
     nodelist = sizeDict.keys()
@@ -466,7 +513,7 @@ def draw_IS_w_recomb_graph(G, alpha = .2, base_node_size = 10):
         #pos = nx.spring_layout(G)
         posMap = nx.spring_layout
     pos = node_positions(G,componentMap = posMap)
-    nx.draw(G, pos = pos, node_color = 'blue', width = 0.2, nodelist = nodelist, node_size = sizes, alpha = alpha, linewidths = 0.0)
+    nx.draw(G, pos = pos, node_color = 'blue', width = 0.2, nodelist = nodelist, node_size = sizes, alpha = alpha, linewidths = 0.0, labels = labels, font_size = font_size)
     #nx.draw(G, pos = pos, node_color = G.nodes(),vmin = 0.5,vmax = 0.6, width = 0.2, nodelist = nodelist, node_size = sizes, alpha = alpha)
 
 
